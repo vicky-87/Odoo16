9d3ae027a47a20944bf2e8c81bd2b4ac5b8b8729
Ahmad Rofikil Khoiri <ahmadrofikil46@gmail.com>
2024-04-29 10:40:48 +0700

    .

--- cdn_training/models/res_partner.py
+++ cdn_training/models/res_partner.py
@@ -3,7 +3,7 @@ from odoo import models, fields, api
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    jenis_kel       = fields.Selection(string='Jenis Kelamin', selection=[('L', 'Laki-laki'), ('P', 'Prempuan')])
+    jenis_kel       = fields.Selection(string='Jenis Kelamin', selection=[('L', 'Laki-laki'), ('P', 'Perempuan')])
     propinsi_id     = fields.Many2one(comodel_name='ref.propinsi', string='Propinsi')
     kota_id         = fields.Many2one(comodel_name="ref.kota",  string="Kota")
     kecamatan_id    = fields.Many2one(comodel_name="ref.kecamatan",  string="Kecamatan")

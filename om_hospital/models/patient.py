7080eeba143f1aa378bc7271530e7b5ba8b28616
Ahmad Rofikil Khoiri <ahmadrofikil46@gmail.com>
2024-04-30 10:44:16 +0700

    Add Filters And Group By

--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -5,7 +5,8 @@ class HospitalPatient(models.Model):
     _name = 'hospital.patient'
     _description = 'Hospital Patient'
 
-    name = fields.Char(string='Name')
+    name = fields.Char(string='Patient Name')
+    ref = fields.Char(string='Reference')
     age = fields.Integer(string='Age')
     gender = fields.Selection(string='Gender', selection=[('male', 'Male'), ('female', 'Female'),])
     

74771e631b794a2f8b187848082bbb41f276f70b
Ahmad Rofikil Khoiri <ahmadrofikil46@gmail.com>
2024-05-01 01:53:04 +0700

    Add Enable Tracking For Fields In Odoo

--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -6,9 +6,9 @@ class HospitalPatient(models.Model):
     _description = 'Hospital Patient'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    name = fields.Char(string='Patient Name')
+    name = fields.Char(string='Patient Name', tracking=True)
     ref = fields.Char(string='Reference')
-    age = fields.Integer(string='Age')
-    gender = fields.Selection(string='Gender', selection=[('male', 'Male'), ('female', 'Female'),])
+    age = fields.Integer(string='Age', tracking=True)
+    gender = fields.Selection(string='Gender', selection=[('male', 'Male'), ('female', 'Female')], tracking=True)
     active = fields.Boolean(string='Active', default=True)
    

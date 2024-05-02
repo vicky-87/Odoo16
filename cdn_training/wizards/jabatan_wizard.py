e28e5d515976df2545a5001eafb8f3521ddb5183
Ahmad Rofikil Khoiri <ahmadrofikil46@gmail.com>
2024-05-02 16:04:28 +0700

    changed name pejabat in wizard

--- cdn_training/wizards/jabatan_wizard.py
+++ cdn_training/wizards/jabatan_wizard.py
@@ -9,6 +9,6 @@ class JabatanWizard(models.Model):
 
     def update_jabatan(self):
         self.pejabat_id.write({'jabatan_id': self.jabatan_id.id})
-        self.jabatan_id.write({'pejabat_id': self.pejabat_id.id})
+        self.jabatan_id.write({'pejabat': self.pejabat_id.id})
 
         return {'type': 'ir.actions.act_window_close'}

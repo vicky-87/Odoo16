4bd2f9265e4129088882946c343e62416a9a2941
Ahmad Rofikil Khoiri <ahmadrofikil46@gmail.com>
2024-05-03 16:21:38 +0700

    Add Onchange Functions In Odoo

--- om_hospital/models/appointment.py
+++ om_hospital/models/appointment.py
@@ -10,3 +10,8 @@ class HospitalAppointment(models.Model):
     gender = fields.Selection(related='patient_id.gender')
     appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
     booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today)
+    ref = fields.Char(string='Reference')
+
+    @api.onchange('patient.id')
+    def onchange_patient_id(self):
+        self.ref = self.patient_id.ref

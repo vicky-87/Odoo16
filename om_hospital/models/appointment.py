53a8ff5ce3ac3b9dcebd26455ad87fe7f00454fd
Ahmad Rofikil Khoiri <ahmadrofikil46@gmail.com>
2024-05-03 14:42:03 +0700

    Add Related Fields In Odoo

--- om_hospital/models/appointment.py
+++ om_hospital/models/appointment.py
@@ -7,5 +7,6 @@ class HospitalAppointment(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
     patient_id = fields.Many2one('hospital.patient', string='Patient')
-    appointment_time = fields.Datetime(string='Appointment Time')
-    booking_date = fields.Date(string='Booking Date')
+    gender = fields.Selection(related='patient_id.gender')
+    appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
+    booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today)

fb710b4a4335766df8b014b738e346c6bbad0a8d
Ahmad Rofikil Khoiri <ahmadrofikil46@gmail.com>
2024-05-05 22:10:10 +0700

    Add Rec Name In Odoo

--- om_hospital/models/appointment.py
+++ om_hospital/models/appointment.py
@@ -5,6 +5,7 @@ class HospitalAppointment(models.Model):
     _name = 'hospital.appointment'
     _description = 'Hospital Appointment'
     _inherit = ['mail.thread', 'mail.activity.mixin']
+    _rec_name = 'patient_id'
 
     patient_id = fields.Many2one('hospital.patient', string='Patient')
     gender = fields.Selection(related='patient_id.gender')
--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -6,6 +6,7 @@ class HospitalPatient(models.Model):
     _name = 'hospital.patient'
     _description = 'Hospital Patient'
     _inherit = ['mail.thread', 'mail.activity.mixin']
+    # _rec_name = 'name'
 
     name = fields.Char(string='Patient Name', tracking=True)
     date_of_birth = fields.Date(string='Date Of Birth')
@@ -13,6 +14,7 @@ class HospitalPatient(models.Model):
     age = fields.Integer(string='Age', compute='_compute_age' ,tracking=True)
     gender = fields.Selection(string='Gender', selection=[('male', 'Male'), ('female', 'Female')], tracking=True)
     active = fields.Boolean(string='Active', default=True)
+    appointment_id = fields.Many2one('hospital.appointment', string='Appointments')
    
     @api.depends('date_of_birth')
     def _compute_age(self):

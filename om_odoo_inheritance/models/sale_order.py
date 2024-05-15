ddd7029022b4e7d2be4ee3db48078428e6ad5f17
Ahmad Rofikil Khoiri <ahmadrofikil46@gmail.com>
2024-05-15 22:40:51 +0700

    Inherit a Function in Odoo

--- om_odoo_inheritance/models/sale_order.py
+++ om_odoo_inheritance/models/sale_order.py
@@ -4,3 +4,8 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     confirmed_user_id = fields.Many2one('res.users', string='Confirmed User')
+
+    def action_confirm(self):
+        super(SaleOrder, self).action_confirm()
+        print("Success......................")
+        self.confirmed_user_id = self.env.user.id
